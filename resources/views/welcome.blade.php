2b2fdc58b5ab90f2ff7faa3e8618701cbd5cb843
Javigranillo <Javiergranillob@gmail.com>
2024-10-31 18:26:00 -0300

    agregamos una imagen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,17 @@
-<h1>Aqui empezamos nuestro proyecto
-</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <h1>Pagina de Inicio</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, vel repudiandae nemo aperiam quisquam eum unde! Officia molestiae, sapiente ipsam aspernatur nesciunt libero voluptas eaque vel placeat, beatae excepturi nemo.</p>
+    <p>Lorem, ipsum dolor.</p>
+    <img src="https://i.ytimg.com/vi/q7b4f9d813I/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLC9QyArj1IQ7VCbaEvblFUoO82WBQ" alt="redbull">
+</body>
+<footer>
+    
+</footer>
+</html>
