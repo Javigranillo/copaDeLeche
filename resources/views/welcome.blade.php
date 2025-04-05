9f575cbef0d807163fc56129a1a16b4adaa0e4cb
Marcos <nieto.marcos.ed@gmail.com>
2025-04-04 21:08:01 -0300

    Creamos un MCR de Escuela

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,25 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" >
-    <title>Inicio</title>
+    <link rel="stylesheet" href="">
+    <title>Copa de leche</title>
 </head>
 <body>
-    <h1>Pagina de Inicio</h1>
-    <br> <hr>
-    <p style="background-color:red">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, vel repudiandae nemo aperiam quisquam eum unde! Officia molestiae, sapiente ipsam aspernatur nesciunt libero voluptas eaque vel placeat, beatae excepturi nemo.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, vel repudiandae nemo aperiam quisquam eum unde! Officia molestiae, sapiente ipsam aspernatur nesciunt libero voluptas eaque vel placeat, beatae excepturi nemo.
-    </p>
-    <br> <hr>
-
-   <p>Lorem, ipsum dolor.</p>
-   <br> <hr>
-    <img src="https://i.ytimg.com/vi/q7b4f9d813I/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLC9QyArj1IQ7VCbaEvblFUoO82WBQ" alt="redbull">
-    <br> <hr>
-
-    <input type="button" value="enviar">
+    <script>   </script>
 </body>
-<footer>
-    
-</footer>
 </html>
